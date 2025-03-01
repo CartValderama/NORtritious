d249c1540ef8e6275418a9a3b4b7464a623985b0
Kjell Wiggo Abrahamsen <s374924@oslomet.no>
2025-03-01 14:53:45 +0100

    Update ProductTable.tsx

--- Frontend/src/products/ProductTable.tsx
+++ Frontend/src/products/ProductTable.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Table, Button, Container, Row, Col, Card } from 'react-bootstrap';
-import { Product } from '../types/product';
-import { Link } from 'react-router-dom';
-import '../css/ProductTable.css';
-import API_URL from '../apiConfig';
-import CollapseCard from '../shared/CollapseCard';
-import ClaimsView from '../shared/ClaimsView';
+import React, { useState } from "react";
+import { Table, Button, Container, Row, Col, Card } from "react-bootstrap";
+import { Product } from "../types/product";
+import { Link } from "react-router-dom";
+import "../css/ProductTable.css";
+import API_URL from "../apiConfig";
+import CollapseCard from "../shared/CollapseCard";
+import ClaimsView from "../shared/ClaimsView";
 
 interface ProductTableProps {
   products: Product[];
@@ -13,13 +13,17 @@ interface ProductTableProps {
   onProductDeleted: (productId: number) => void;
 }
 
-
-const ProductTable: React.FC<ProductTableProps> = ({ products, onProductDeleted }) => {
-  const [showId, setShowId] = useState<boolean>(false);  
+const ProductTable: React.FC<ProductTableProps> = ({
+  products,
+  onProductDeleted,
+}) => {
+  const [showId, setShowId] = useState<boolean>(false);
   const [showNutrition, setShowNutrition] = useState<boolean>(false);
   const [showClaims, setShowClaims] = React.useState<boolean>(false);
-  const [claimsContent, setClaimsContent] = React.useState<string>('');
-  const [visibleNutrition, setVisibleNutrition] = useState<{ [key: number]: boolean }>({});
+  const [claimsContent, setClaimsContent] = React.useState<string>("");
+  const [visibleNutrition, setVisibleNutrition] = useState<{
+    [key: number]: boolean;
+  }>({});
   const [selectedProduct, setSelectedProduct] = useState<any>(null);
   const [showType, setShowType] = useState<boolean>(false);
 
@@ -38,10 +42,10 @@ const ProductTable: React.FC<ProductTableProps> = ({ products, onProductDeleted 
 
   // Formaterer innholdet i kortet
   const formatContent = (content: string) => {
-    return content.split('\n').map((line, index) => (
-      // For hver linje i innholdet, returnes en paragraf via HTML, 
+    return content.split("\n").map((line, index) => (
+      // For hver linje i innholdet, returnes en paragraf via HTML,
       // dangerouslySetInnerHTML er innerHTML i React
-      <p key={index} dangerouslySetInnerHTML={{__html: line }}/>
+      <p key={index} dangerouslySetInnerHTML={{ __html: line }} />
     ));
   };
 
@@ -49,42 +53,41 @@ const ProductTable: React.FC<ProductTableProps> = ({ products, onProductDeleted 
     <Container fluid>
       <Row className="mb-3">
         <Col>
-        <Button 
-            onClick={() => setShowId(!showId)} 
+          <Button
+            onClick={() => setShowId(!showId)}
             className="btn btn-primary me-2"
             size="sm"
           >
             <i className="bi bi-list-ol"></i>
-            {showId ? ' Gjem' : ' Vis'}
+            {showId ? " Gjem" : " Vis"}
           </Button>
-          <Button 
-            onClick={() => setShowType(!showType)} 
+          <Button
+            onClick={() => setShowType(!showType)}
             className="btn btn-primary me-2"
             size="sm"
           >
             <i className="bi bi-tag"></i>
-            {showType ? ' Gjem' : ' Vis'}
+            {showType ? " Gjem" : " Vis"}
           </Button>
-          <Button 
-            onClick={() => setShowNutrition(!showNutrition)} 
+          <Button
+            onClick={() => setShowNutrition(!showNutrition)}
             className="btn btn-primary"
             size="sm"
           >
-            <i className="bi bi-clipboard-data"></i> 
-            {showNutrition ? ' Gjem' : ' Vis'}
+            <i className="bi bi-clipboard-data"></i>
+            {showNutrition ? " Gjem" : " Vis"}
           </Button>
-
         </Col>
       </Row>
       <Row>
         <Col>
           <div className="table-responsive">
-            <Table 
-              striped 
-              bordered 
-              hover 
-              className="shadow-sm" 
-              style={{ backgroundColor: 'white' }}
+            <Table
+              striped
+              bordered
+              hover
+              className="shadow-sm"
+              style={{ backgroundColor: "white" }}
             >
               <thead className="bg-light">
                 <tr>
@@ -92,82 +95,138 @@ const ProductTable: React.FC<ProductTableProps> = ({ products, onProductDeleted 
                   <th className="align-middle text-center">Navn</th>
                   <th className="align-middle text-center">Bilde</th>
                   <th className="align-middle text-center">Gruppe</th>
-                  {showType && <th className="align-middle text-center">Type</th>}
-                  {showNutrition && <th className="align-middle">Næringsmiddel pr. 100 g/ml</th>}
-                  <th className='align-middle text-center'>Nøkkelhullet</th>
-                  <th className='align-middle text-center'>EFSA Ernæringspåstander</th>
-                  <th className='align-middle text-center'>EFSA Helsepåstander</th>
+                  {showType && (
+                    <th className="align-middle text-center">Type</th>
+                  )}
+                  {showNutrition && (
+                    <th className="align-middle">Næringsmiddel pr. 100 g/ml</th>
+                  )}
+                  <th className="align-middle text-center">Nøkkelhullet</th>
+                  <th className="align-middle text-center">
+                    EFSA Ernæringspåstander
+                  </th>
+                  <th className="align-middle text-center">
+                    EFSA Helsepåstander
+                  </th>
                   <th className="align-middle text-center">Behandling</th>
                 </tr>
               </thead>
               <tbody>
-                {products.map(product => (
+                {products.map((product) => (
                   <tr key={product.productId}>
                     {showId && (
                       <td className="align-middle">{product.productId}</td>
                     )}
                     <td className="align-middle fw-bold">
-                      <Link 
-                        to={`/productdetails/${product.productId}`}
-                        className='text-decoration-none'
+                      <Link
+                        to={`/products/details/${product.productId}`}
+                        className="text-decoration-none"
                       >
                         {product.name}
                       </Link>
                     </td>
 
-                      <td className="align-middle text-center">
-                        <Link 
-                          to={`/productdetails/${product.productId}`}
-                          className='text-decoration-none'
-                        >
-                        <img 
-                          src={`${API_URL}/images/${product.imageUrl}`} 
-                          alt={product.name} 
-                          className="rounded" 
-                          style={{ maxWidth: '120px', height: 'auto' }} 
-                        />
-                        </Link>
-                      </td>
-                    
-                    
-                    <td className="align-middle">{formatContent(product.group)}</td>
-                    {showType && <td className="align-middle small">{formatContent(product.type)}</td>}
-
-                    {showNutrition && <td className="align-middle small">
-                    Energi: {product.calories}<br/>
-                    Fett: {product.fat}<br/>
-                    Mettet fett: {product.satFat}<br/>
-                    Karbo: {product.carbs}<br/>
-                    Nat sukker: {product.natSugar}<br/>
-                    Tilsatt sukker: {product.addedSugar}<br/>
-                    Fiber: {product.fiber}<br/>
-                    Protein: {product.protein}<br/>
-                    Salt: {product.salt}</td>
-                    }
                     <td className="align-middle text-center">
-                    <img 
-                          src={product.hasNokkelhullet ? `${API_URL}/images/circle-keyhole-logo.png` : `${API_URL}/images/ban_keyhole.png`}	 
-                          alt={product.hasNokkelhullet ? 'Oppfyller Nøkkelhullet' : 'Oppfyller ikke Nøkkelhullet'}
-                          style={{ width: '50px', height: '50px', padding: '2px' }} 
-                          className="rounded" 
+                      <Link
+                        to={`/productdetails/${product.productId}`}
+                        className="text-decoration-none"
+                      >
+                        <img
+                          src={`${API_URL}/images/${product.imageUrl}`}
+                          alt={product.name}
+                          className="rounded"
+                          style={{ maxWidth: "120px", height: "auto" }}
                         />
-                      
+                      </Link>
+                    </td>
+
+                    <td className="align-middle">
+                      {formatContent(product.group)}
+                    </td>
+                    {showType && (
+                      <td className="align-middle small">
+                        {formatContent(product.type)}
+                      </td>
+                    )}
+
+                    {showNutrition && (
+                      <td className="align-middle small">
+                        Energi: {product.calories}
+                        <br />
+                        Fett: {product.fat}
+                        <br />
+                        Mettet fett: {product.satFat}
+                        <br />
+                        Karbo: {product.carbs}
+                        <br />
+                        Nat sukker: {product.natSugar}
+                        <br />
+                        Tilsatt sukker: {product.addedSugar}
+                        <br />
+                        Fiber: {product.fiber}
+                        <br />
+                        Protein: {product.protein}
+                        <br />
+                        Salt: {product.salt}
+                      </td>
+                    )}
+                    <td className="align-middle text-center">
+                      <img
+                        src={
+                          product.hasNokkelhullet
+                            ? `${API_URL}/images/circle-keyhole-logo.png`
+                            : `${API_URL}/images/ban_keyhole.png`
+                        }
+                        alt={
+                          product.hasNokkelhullet
+                            ? "Oppfyller Nøkkelhullet"
+                            : "Oppfyller ikke Nøkkelhullet"
+                        }
+                        style={{
+                          width: "50px",
+                          height: "50px",
+                          padding: "2px",
+                        }}
+                        className="rounded"
+                      />
                     </td>
 
                     <td className="align-middle text-center">
-                      <Button variant='outline-primary'>
-                      <img
-                        src={product.hasEfsaNutrition ? `${API_URL}/images/efsaLogoGreen.png` : `${API_URL}/images/efsaLogoBlack.png`}
-                        alt={product.hasEfsaNutrition ? 'Has EFSA Nutrition' : 'No EFSA Nutrition'}
-                        style={{ width: '70px', height: '70px', cursor: 'pointer'  }}
-                        onClick={() => handleToggleNutrition(product.productId)}
-                      />
+                      <Button variant="outline-primary">
+                        <img
+                          src={
+                            product.hasEfsaNutrition
+                              ? `${API_URL}/images/efsaLogoGreen.png`
+                              : `${API_URL}/images/efsaLogoBlack.png`
+                          }
+                          alt={
+                            product.hasEfsaNutrition
+                              ? "Has EFSA Nutrition"
+                              : "No EFSA Nutrition"
+                          }
+                          style={{
+                            width: "70px",
+                            height: "70px",
+                            cursor: "pointer",
+                          }}
+                          onClick={() =>
+                            handleToggleNutrition(product.productId)
+                          }
+                        />
                       </Button>
-                      
-                      {visibleNutrition[product.productId] && product.hasEfsaNutrition && (
-                        <Card><div><strong>Ernæringspåstander:<br/></strong>{product.hasEfsaNutrition}</div></Card>
 
-                      )}
+                      {visibleNutrition[product.productId] &&
+                        product.hasEfsaNutrition && (
+                          <Card>
+                            <div>
+                              <strong>
+                                Ernæringspåstander:
+                                <br />
+                              </strong>
+                              {product.hasEfsaNutrition}
+                            </div>
+                          </Card>
+                        )}
                     </td>
 
                     <td className="align-middle text-center">
@@ -179,14 +238,16 @@ const ProductTable: React.FC<ProductTableProps> = ({ products, onProductDeleted 
                       <Button
                         variant="outline-primary"
                         size="sm"
-                        onClick={() => handleShowClaims(product.hasEfsaHealth, product)}
+                        onClick={() =>
+                          handleShowClaims(product.hasEfsaHealth, product)
+                        }
                       >
                         Full oversikt
                       </Button>
                     </td>
 
                     <td className="align-middle text-center">
-                      <Link 
+                      <Link
                         to={`/products/updateCalculator/${product.productId}`}
                         className="btn btn-outline-primary btn-sm"
                       >
@@ -198,22 +259,23 @@ const ProductTable: React.FC<ProductTableProps> = ({ products, onProductDeleted 
                         size="sm"
                         onClick={() => onProductDeleted(product.productId)}
                       >
-                        <i className='bi bi-trash'></i> Fjern
+                        <i className="bi bi-trash"></i> Fjern
                       </Button>
-                      </td>
+                    </td>
                   </tr>
                 ))}
-              </tbody>              
+              </tbody>
             </Table>
           </div>
         </Col>
       </Row>
       {selectedProduct && (
-        <ClaimsView 
-        show={showClaims} 
-        onHide={() => setShowClaims(false)} 
-        content={claimsContent} 
-        product={selectedProduct} />
+        <ClaimsView
+          show={showClaims}
+          onHide={() => setShowClaims(false)}
+          content={claimsContent}
+          product={selectedProduct}
+        />
       )}
     </Container>
   );
