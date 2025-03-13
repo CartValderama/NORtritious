42941987fe6408ad8b23803e9dfd2df2d09a0d05
Mats August Schmid Olsen <s360784@oslomet.no>
2025-03-13 13:38:18 +0000

    Edit vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'public', // Output dir for build files
+  },
+  /*
   server: {
     port: 5173, // Sett porten du ønsker
   },
+  */
 })
