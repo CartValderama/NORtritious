abdfd8641e813730786ac20ad0c3a3b20a89c8ee
Mats August Schmid Olsen <s360784@oslomet.no>
2025-03-13 21:49:38 +0000

    Update file apiConfig.tsx

--- Frontend/src/apiConfig.tsx
+++ Frontend/src/apiConfig.tsx
@@ -1,3 +1,3 @@
-const API_URL = 'https://d231-2a01-799-de3-1300-9003-a242-5537-caad.ngrok-free.app';//import.meta.env.VITE_API_URL;//'https://localhost:7064';
+const API_URL = 'https://7b06-2a01-799-de3-1300-9003-a242-5537-caad.ngrok-free.app/';//import.meta.env.VITE_API_URL;//'https://localhost:7064';
 
 export default API_URL;
